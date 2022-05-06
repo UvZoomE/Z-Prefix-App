d45dc29771d24c66837ffbf68ded627f0fac111b
uvzoome <uvzumaya@gmail.com>
2022-05-05 21:53:18 -0600

    set up knex express server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 8081
+const PORT = process.env.PORT || 8081
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV || 'development']);
+
+app.get('/users', (req, res) => {
+  knex
+  .select('*')
+  .from('new_user')
+  .then(data => res.status(200).json(data))
+  .catch(err =>
+    res.status(404).json({
+      message:
+      'The data you are looking for could not be found. Please try again'
+    }))
+})
+
+app.listen(PORT, () => {
+  console.log(`Listening at port ${PORT}`)
+})
